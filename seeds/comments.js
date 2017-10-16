f64118947f2ba23070a3c37155f144ba5b01572d
Leo McClung <murraythemoose@gmail.com>
2017-10-16 15:06:42 -0600

    finished hard coding content into seeds for testing

--- seeds/comments.js
+++ seeds/comments.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('comments').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('comments').insert([
+        {id: 1, title: 'comment1', body: 'this sucks', users_id: 1, post_id: 1},
+        {id: 2, title: 'comment2', body: 'this sucks too', users_id: 2, post_id: 2},
+        {id: 3, title: 'comment3', body: 'baller', users_id: 3, post_id: 3}
       ]);
     });
 };
--- seeds/posts.js
+++ seeds/posts.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('posts').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('posts').insert([
+        {id: 1, title: 'apost', body: 'stuff', users_id: 1},
+        {id: 2, title: 'anotherpost', body: 'stuff', users_id: 2},
+        {id: 3, title: 'moreposts', body: 'stuff', users_id: 3}
       ]);
     });
 };
