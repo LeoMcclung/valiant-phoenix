ec7cf08956625d5ccf7a5f1bd13fdad7b139c286
Leo McClung <murraythemoose@gmail.com>
2017-10-16 14:57:25 -0600

    hard coded content into seed files for testing

--- seeds/users.js
+++ seeds/users.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {id: 1, username: 'bobsd69', password: 'abc123', name: 'bob', email: 'bobwars@hooo.com'},
+        {id: 2, username: 'maryjane420', password: 'dankassshit', name: 'steve', email: 'sirsmokesalot@mail.net'},
+        {id: 3, username: 'thedude', password: 'epicpass', name: 'moe', email: 'moe@moe.com'}
       ]);
     });
 };
