36dceab83a4d458645897a763b93d77433085f86
Leo McClung <murraythemoose@gmail.com>
2017-10-16 14:39:29 -0600

    finished migrate files

--- migrations/20171016141104_posts.js
+++ migrations/20171016141104_posts.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('posts', function(table){
+        table.increments(id).primary();
+        table.string('title');
+        table.string('body');
+        table.integer('users_id').references('uid').inTable('users');
+        table.dateTime('postDate');
+    })
   
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('posts');
   
 };
--- migrations/20171016141111_comments.js
+++ migrations/20171016141111_comments.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('comments', function(table){
+        table.increments('id').primary();
+        table.string('title');
+        table.string('body');
+        table.integer('users_id').references('uid').inTable('users');
+        table.integer('post_id').references('id').inTable('posts');
+        table.dateTime('postDate');
+    })
   
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('comments');
   
 };
